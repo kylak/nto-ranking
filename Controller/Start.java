4aa885a887efd0a57d09b6c1f4d9e4372c088379
kylak <gustavberloty2@gmail.com>
2020-07-29 17:36:43 +0200

    Start.java synchronized.

--- Controller/Start.java
+++ Controller/Start.java
@@ -15,89 +15,96 @@ import java.io.IOException;
 public class Start {
     
     public static void main(String[] args) {
-        
-        long startTimer = System.currentTimeMillis();
-        System.out.println("\n");
-        /*
-        enum CriticalTexts {
-            WH, NA28, SBL, RP, KJTR, ST//, BHP
-        }
-        */
-        final String greekText = "KJTR";
-        final SyntacticRoles[] roleToKeep = new SyntacticRoles[4];
-        roleToKeep[0] = new SyntacticRoles(SyntacticRoles.sign.N); // Nouns
-        roleToKeep[1] = new SyntacticRoles(SyntacticRoles.sign.A); // Adjectives
-        roleToKeep[2] = new SyntacticRoles(SyntacticRoles.sign.V); // Verbs
-        roleToKeep[3] = new SyntacticRoles(SyntacticRoles.sign.D); // Adverbs
-        
-        // Obtaining        
-        ExecutorService threadpool0 = Executors.newCachedThreadPool();
-        long start0 = System.currentTimeMillis();
-		Future<GetPassages> futureTask0 = threadpool0.submit(() -> new GetPassages("List_of_verses.csv", greekText));
-		while (!futureTask0.isDone()) {/* wait task1 to be completed */}
-		long time0 = System.currentTimeMillis() - start0;
-		System.out.println("\nData obtained in approximatively " + (int)((float)(time0/1000.0) / 60) + "min" + (int)((float)(time0/1000.0) % 60.0) + "s.");
-		GetPassages fds = null;
-		try {
-			fds = futureTask0.get(); 
-		}
-		catch (InterruptedException | ExecutionException e) {}
-		final GetPassages findings = fds;
-		threadpool0.shutdown();
-        
-        
-        // Classifiying
-        ExecutorService threadpool = Executors.newCachedThreadPool();
-		long start = System.currentTimeMillis();
-		Future<Classify> futureTask = threadpool.submit(() -> new Classify(findings.passages, roleToKeep));
-		while (!futureTask0.isDone()) {/* wait task1 to be completed */}
-		long time = System.currentTimeMillis() - start;
-		System.out.println("Data classified in approximatively " + time + "ms.");
-		Classify cVerses = null;
-		try {
-			cVerses = futureTask.get();
-			/*for(HashMap.Entry<GreekStrong, ArrayList<Verse>> entry : cVerses.interestingClassifiedVerses.entrySet()) {
-				System.out.println(entry.getKey().strongNumber);
-				for (Verse temp : entry.getValue()) {
-					int i = 0;
-                    for (Float strongNmbr : temp.strongNumbers) {
-                    	System.out.println(temp.ref + ": " + temp.text);
-                    	System.out.println("Morph: " + temp.morph.get(i).charAt(0));
-                    	i++;
-                    }
-                }
-			}*/
-		}
-		catch (InterruptedException | ExecutionException e) {}
-		final Classify classifiedVerses = cVerses;
-		threadpool.shutdown();
-        
-        
-        // Generating
-        ExecutorService threadpool2 = Executors.newCachedThreadPool();
-        final Object process1 = new Object();
-        long start1;
-        Future<GenerateFile> futureTask2;
+    
+    	final Object process1 = new Object();
+    	long startTimer;
+    	String greekText;
+    	SyntacticRoles[] roleToKeep;
+    	GetPassages findings;
+    	Classify classifiedVerses;
+    	
         synchronized(process1) {
-			futureTask2 = threadpool2.submit(() -> new GenerateFile(classifiedVerses, greekText, roleToKeep));
-			start1 = System.currentTimeMillis();
+			startTimer = System.currentTimeMillis();
+			System.out.println("\n");
+			/*
+			enum CriticalTexts {
+				WH, NA28, SBL, RP, KJTR, ST//, BHP
+			}
+			*/
+			greekText = "KJTR";
+			roleToKeep = new SyntacticRoles[4];
 		}
-		while (!futureTask0.isDone()) {/* wait task1 to be completed */}
-		GenerateFile gf = null;
-		try {
-			gf = futureTask2.get();
-			synchronized(process1) {
-				gf.generateFiles(findings.passagesTranslated);
+		
+		synchronized(process1) {
+			roleToKeep[0] = new SyntacticRoles(SyntacticRoles.sign.N); // Nouns
+			roleToKeep[1] = new SyntacticRoles(SyntacticRoles.sign.A); // Adjectives
+			roleToKeep[2] = new SyntacticRoles(SyntacticRoles.sign.V); // Verbs
+			roleToKeep[3] = new SyntacticRoles(SyntacticRoles.sign.D); // Adverbs
+		}
+        
+        // Obtaining
+        synchronized(process1) {
+        	long start0, time0;
+        	final Object process11 = new Object();
+            synchronized(process11) {
+				start0 = System.currentTimeMillis();
+				findings = new GetPassages("List_of_verses.csv", greekText);
+			}
+			synchronized(process11) {
+				time0 = System.currentTimeMillis() - start0;
+			}
+			synchronized(process11) {
+				System.out.println("\nData obtained in approximatively " + (int)((float)(time0/1000.0) / 60) + "min" + (int)((float)(time0/1000.0) % 60.0) + "s.");
 			}
 		}
-		catch (InterruptedException | ExecutionException | FileNotFoundException | UnsupportedEncodingException t) {}
-		catch (IOException e) {}
-		long time1 = System.currentTimeMillis() - start1;
-		double seconds1 = time1 / 1000.0;
-		System.out.println("Classified data generated in approximatively " + (float)seconds1 + "s.");
-		threadpool2.shutdown();
 		
-		long endTimer = System.currentTimeMillis() - startTimer;
-		System.out.println("Entire program executed in approximatively " + (int)((float)(endTimer/1000.0) / 60) + "min" + (int)((float)(endTimer/1000.0) % 60.0) + "s.\n");
+        // Classifiying
+        synchronized(process1) {
+			long start, time;
+			final Object process11 = new Object();
+			synchronized(process11) {
+				start = System.currentTimeMillis();
+				classifiedVerses = new Classify(findings.passages, roleToKeep);
+			}
+			synchronized(process11) {
+				time = System.currentTimeMillis() - start;
+			}
+			synchronized(process11) {
+				System.out.println("Data classified in approximatively " + time + "ms.");
+			}
+		}
+
+        // Generating
+        synchronized(process1) {
+        
+			long start1, time1, endTimer;
+			double seconds1;
+			GenerateFile gf;
+			final Object process11 = new Object();
+			
+			synchronized(process11) {
+				gf = new GenerateFile(classifiedVerses, greekText, roleToKeep);
+				start1 = System.currentTimeMillis();
+			}
+			synchronized(process11) {
+				try {
+					gf.generateFiles(findings.passagesTranslated);
+				} catch (IOException t) {}
+				time1 = System.currentTimeMillis() - start1;
+			}
+			synchronized(process11) {
+				seconds1 = time1 / 1000.0;
+			}
+			synchronized(process11) {
+				System.out.println("Classified data generated in approximatively " + (float)seconds1 + "s.");
+			}
+			
+			synchronized(process11) {
+				endTimer = System.currentTimeMillis() - startTimer;
+			}
+			synchronized(process11) {
+				System.out.println("Entire program executed in approximatively " + (int)((float)(endTimer/1000.0) / 60) + "min" + (int)((float)(endTimer/1000.0) % 60.0) + "s.\n");
+			}
+		}
 	}
 }
